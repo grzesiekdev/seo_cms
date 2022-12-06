71c3871b6fb5dad99602084a6e6a3d8e1e48f70b
Grzesiek <grzesiekb@onet.pl>
2022-12-06 14:19:50 +0100

    Added basic List of Users for admin panel

--- src/Controller/Admin/PanelController.php
+++ src/Controller/Admin/PanelController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Repository\PageRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,8 +15,23 @@ class PanelController extends AbstractController
     #[Route('/admin', name: 'admin_panel')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        //* @var \App\Entity\User $user */
+        $user = $this->getUser();
+
         return $this->render('admin/panel/index.html.twig', [
-            'controller_name' => 'PanelController',
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/admin/users', name: 'admin_panel_users')]
+    public function users(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/panel/users.html.twig', [
+            'users' => $users,
         ]);
     }
 }
