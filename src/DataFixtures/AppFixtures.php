3de56aa61838c8a65a79ecd2a80805f8c196f96e
Grzesiek <grzesiekb@onet.pl>
2023-02-02 22:59:37 +0100

    Added fixtures, fixed typo in security

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,19 +2,48 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Menu;
+use App\Entity\Page;
+use App\Entity\Settings;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher) {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $menu = new Menu();
-        $menu->setLabel('Test 1');
-        $menu->setPageOrder(1);
-        $manager->persist($menu);
+        $page = new Page();
+        $page->setTitle('Home');
+        $page->setName("Home");
+        $page->setDescription("Test");
+        $page->setContent("Welcome");
+        $page->setCreationDate(new \DateTime());
+        $page->setSlug('home');
+        $page->setIsHome(1);
+        $page->setRobotsSettings('index, follow');
+        $page->setAlias('home');
 
+        $settings = new Settings();
+        $settings->setSettingName('logo_path');
+        $settings->setSettingValue('63d8f4690a75a.png');
+
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $admin,
+                'test12'
+            )
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($page);
+        $manager->persist($settings);
+        $manager->persist($admin);
         $manager->flush();
     }
 }
